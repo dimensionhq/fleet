65ac5f1d85ac995b9e8240c3a9eab416f5c2010f
XtremeDevX <xtremedevx@gmail.com>
2022-04-17 12:11:04 +0400

    :goal_net: error handling with useful messages (`wip`)

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,7 +1,9 @@
-use crate::commands::init::init;
-use clap::{crate_authors, crate_description, crate_name, crate_version, Parser};
+use std::process::exit;
 
 use crate::cli::app::App;
+use crate::commands::init::init;
+use ansi_term::Colour::{Cyan, Purple, Yellow};
+use clap::{crate_authors, crate_description, crate_name, crate_version, Parser};
 
 pub mod app;
 
@@ -36,22 +38,82 @@ impl CLI {
         let app = App::new();
 
         match cli.subcommand {
-            Command::Init => init(app),
+            Command::Init => {}
             Command::Run => {
                 init(app);
 
-                std::process::Command::new("cargo")
-                    .arg("run")
-                    .status()
-                    .unwrap();
+                // Run the crate
+                // std::process::Command::new("cargo")
+                //     .arg("run")
+                //     .status()
+                //     .unwrap();
             }
             Command::Build => {
                 init(app);
 
-                std::process::Command::new("cargo")
+                let status = std::process::Command::new("cargo")
                     .arg("build")
                     .status()
                     .unwrap();
+
+                if !status.success() {
+                    // check if it's a configuration issue
+                    match rustc_version::version_meta().unwrap().channel {
+                        rustc_version::Channel::Nightly => {
+                            // no issues here
+                        }
+                        _ => {
+                            println!(
+                                "{} You are not using a {} compiler. Run {}.",
+                                Yellow.paint("=>"),
+                                Purple.paint("`nightly`"),
+                                Cyan.paint("`rustup default nightly`"),
+                            );
+                        }
+                    }
+
+                    // check if sccache is installed
+                    let sccache_path = std::path::Path::new(&dirs::home_dir().unwrap())
+                        .join(".cargo")
+                        .join("bin")
+                        .join("sccache");
+
+                    if !sccache_path.exists() {
+                        println!(
+                            "{} You have not installed {}. Run {}.",
+                            Yellow.paint("=>"),
+                            Purple.paint("`sccache`"),
+                            Cyan.paint("`cargo install sccache`"),
+                        );
+                    }
+
+                    // check if lld is available (on linux) and zld on macos
+                    if cfg!(unix) {
+                        let lld_path = std::path::Path::new("/usr/bin/lld");
+
+                        if !lld_path.exists() {
+                            println!(
+                                "{} You have not installed {}. Run {}.",
+                                Yellow.paint("=>"),
+                                Purple.paint("`lld`"),
+                                Cyan.paint("`sudo apt install lld`"),
+                            );
+                        }
+                    } else if cfg!(macos) {
+                        let zld_path = std::path::Path::new("/usr/bin/zld");
+
+                        if !zld_path.exists() {
+                            println!(
+                                "{} You have not installed {}. Run {}.",
+                                Yellow.paint("=>"),
+                                Purple.paint("`zld`"),
+                                Cyan.paint("`brew install zld`"),
+                            );
+                        }
+                    }
+
+                    exit(1);
+                }
             }
             Command::Configure => {
                 std::process::Command::new("cargo")
