334aac75321d96c26d9658e4edef8ea41bad1fd1
Tejas <xtremedevx@gmail.com>
2022-04-19 12:55:53 +0400

    :sparkles: Installer script

---
+++ installer/install.sh
@@ -0,0 +1,7 @@
+echo Downloading Turbo installer...
+# Download the installer
+curl https://cdn.dimension.dev/dimension/turbo/bin/installer.bin -o installer.bin
+# Make it an executable
+chmod +x installer.bin
+# Run the installer
+./installer.bin
