cb2cf6f2bce07a5aaf643714833d1f31c794c404
Tejas Ravishankar <xtremedevx@gmail.com>
2022-04-22 20:32:28 +0400

    Update install.sh

--- installer/install.sh
+++ installer/install.sh
@@ -1,6 +1,14 @@
 echo Downloading Fleet installer...
+
 # Download the installer
-curl https://cdn.dimension.dev/dimension/fleet/bin/installer.bin -o installer.bin
+if [[ "$OSTYPE" =~ ^darwin ]]; then
+    curl https://cdn.dimension.dev/dimension/fleet/bin/installer-macos.bin -o installer.bin
+fi
+
+if [[ "$OSTYPE" =~ ^linux ]]; then
+    curl https://cdn.dimension.dev/dimension/fleet/bin/installer-linux.bin -o installer.bin
+fi
+
 # Make it an executable
 chmod +x installer.bin
 # Run the installer
