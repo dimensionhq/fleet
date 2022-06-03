b61b1a170356aef815c0bd81a39b5321044b2768
XtremeDevX <xtremedevx@gmail.com>
2022-06-03 20:29:02 +0400

    :construction: structs for bloat analysis

--- src/utils/bloat.rs
+++ src/utils/bloat.rs
@@ -16,3 +16,22 @@ pub struct Crate {
     pub name: String,
     pub size: i64,
 }
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct BloatFunctionAnalysis {
+    #[serde(rename = "file-size")]
+    pub file_size: i64,
+    #[serde(rename = "text-section-size")]
+    pub text_section_size: i64,
+    pub functions: Vec<Function>,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Function {
+    #[serde(rename = "crate")]
+    pub crate_field: String,
+    pub name: String,
+    pub size: i64,
+}
