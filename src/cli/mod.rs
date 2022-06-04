775eab410422701e9fa9f99dad7d1aa1f67876d2
XtremeDevX <xtremedevx@gmail.com>
2022-06-05 00:04:26 +0400

    :zap: ~2x faster `bloat` command

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -20,12 +20,9 @@ use cargo_util::ProcessBuilder;
 use clap::{crate_authors, crate_description, crate_name, crate_version, AppSettings, Parser};
 use colored::Colorize;
 use comfy_table::ContentArrangement;
-use comfy_table::Width::Percentage;
-use comfy_table::{
-    modifiers::UTF8_ROUND_CORNERS, presets::UTF8_FULL, Cell, Color, ColumnConstraint,
-};
+use comfy_table::{modifiers::UTF8_ROUND_CORNERS, presets::UTF8_FULL, Cell, Color};
 use indicatif::{ProgressBar, ProgressStyle};
-use std::io::Read;
+use std::thread::JoinHandle;
 use std::{env, path::PathBuf, process::exit};
 
 use crate::{
@@ -226,6 +223,7 @@ The blazing fast build tool for Rust.
                         .skip(2)
                         .map(std::string::ToString::to_string)
                         .collect();
+
                     // Run the crate
                     let status = std::process::Command::new("cargo")
                         .arg("run")
@@ -305,153 +303,171 @@ The blazing fast build tool for Rust.
                     crate::utils::configure::install_linker(&linker_selected);
                 }
                 "bloat" => {
+                    let mut handles: Vec<JoinHandle<comfy_table::Table>> = vec![];
                     let spinner = ProgressBar::new_spinner();
 
-                    spinner.set_style(ProgressStyle::default_spinner().template("{spinner} {msg}"));
+                    handles.push(std::thread::spawn({
+                        let spinner = spinner.clone();
+                        move || -> comfy_table::Table {
+                            spinner.set_style(
+                                ProgressStyle::default_spinner().template("{spinner} {msg}"),
+                            );
 
-                    spinner.set_message("Initializing".bright_green().to_string());
+                            spinner.set_message("Initializing".bright_green().to_string());
 
-                    spinner.enable_steady_tick(10);
+                            spinner.enable_steady_tick(10);
 
-                    // Run cargo bloat
-                    let mut command = ProcessBuilder::new("cargo");
+                            let mut warning_count: u64 = 0;
+                            let mut error_count: u64 = 0;
 
-                    command
-                        .arg("bloat")
-                        .arg("--crates")
-                        .arg("--message-format=json");
+                            // Run cargo bloat
+                            let mut command = ProcessBuilder::new("cargo");
 
-                    // TODO: run both analysis at the same time
+                            command
+                                .arg("bloat")
+                                .arg("--crates")
+                                .arg("--message-format=json");
 
-                    let mut warning_count = 0;
-                    let mut error_count = 0;
+                            let output = command.exec_with_streaming(
+                                &mut |_| Ok(()),
+                                &mut |on_stderr| {
+                                    let contents = on_stderr.trim().to_string();
 
-                    let output = command.exec_with_streaming(
-                        &mut |_| Ok(()),
-                        &mut |on_stderr| {
-                            let contents = on_stderr.trim().to_string();
+                                    if contents != "" {
+                                        let chunks: Vec<&str> = contents.split(" ").collect();
 
-                            if contents != "" {
-                                let chunks: Vec<&str> = contents.split(" ").collect();
+                                        if contents.starts_with("Compiling") {
+                                            let name = chunks[1].to_string();
 
-                                if contents.starts_with("Compiling") {
-                                    let name = chunks[1].to_string();
+                                            let mut version = chunks[2].to_string();
 
-                                    let mut version = chunks[2].to_string();
+                                            if version.starts_with("v") {
+                                                version.remove(0);
+                                            }
 
-                                    if version.starts_with("v") {
-                                        version.remove(0);
+                                            spinner.set_message(format!(
+                                                "{} ({}{}{})",
+                                                "Compile".bright_cyan(),
+                                                name.bright_yellow(),
+                                                "@".bright_magenta(),
+                                                version.bright_black(),
+                                            ));
+                                        }
+
+                                        if contents.starts_with("warning:") {
+                                            warning_count += 1;
+                                            spinner.set_message(format!(
+                                                "{} ({} {}, {} {})",
+                                                "Check".bright_cyan(),
+                                                warning_count.to_string().bright_magenta(),
+                                                "warnings".bright_yellow(),
+                                                error_count.to_string().bright_red(),
+                                                "errors".bright_yellow(),
+                                            ));
+                                        }
+
+                                        if contents.starts_with("error") {
+                                            error_count += 1;
+                                        }
                                     }
 
-                                    spinner.set_message(format!(
-                                        "{} ({}{}{})",
-                                        "Compile".bright_cyan(),
-                                        name.bright_yellow(),
-                                        "@".bright_magenta(),
-                                        version.bright_black(),
-                                    ));
-                                }
+                                    Ok(())
+                                },
+                                true,
+                            );
 
-                                if contents.starts_with("warning:") {
-                                    warning_count += 1;
-                                    spinner.set_message(format!(
-                                        "{} ({} {}, {} {})",
-                                        "Check".bright_cyan(),
-                                        warning_count.to_string().bright_magenta(),
-                                        "warnings".bright_yellow(),
-                                        error_count.to_string().bright_red(),
-                                        "errors".bright_yellow(),
-                                    ));
-                                }
+                            if output.is_err() {
+                                spinner.finish();
 
-                                if contents.starts_with("error") {
-                                    error_count += 1;
-                                }
+                                // Show all errors in the codebase:
+                                std::process::Command::new("cargo")
+                                    .arg("check")
+                                    .status()
+                                    .unwrap();
+
+                                std::process::exit(1);
                             }
 
-                            Ok(())
-                        },
-                        true,
-                    );
+                            spinner.set_message("Analysing".bright_cyan().to_string());
 
-                    if output.is_err() {
-                        spinner.finish();
+                            let stdout = String::from_utf8(output.unwrap().stdout).unwrap();
 
-                        std::process::Command::new("cargo")
-                            .arg("check")
-                            .status()
+                            let data = serde_json::from_str::<BloatCrateAnalysis>(&stdout).unwrap();
+
+                            let total_size = byte_unit::Byte::from_bytes(data.file_size as u128);
+                            let adjusted_size = total_size.get_appropriate_unit(true);
+
+                            println!("Total Size: {}", adjusted_size.to_string().bright_yellow());
+
+                            let mut crates_table = comfy_table::Table::new();
+
+                            crates_table
+                                .load_preset(UTF8_FULL)
+                                .apply_modifier(UTF8_ROUND_CORNERS)
+                                .set_content_arrangement(ContentArrangement::DynamicFullWidth);
+
+                            crates_table.set_header(vec!["Name", "Size"]);
+
+                            for _crate in data.crates.iter() {
+                                let size = byte_unit::Byte::from_bytes(_crate.size as u128);
+                                let adjusted_size = size.get_appropriate_unit(true);
+
+                                crates_table.add_row(vec![
+                                    Cell::new(_crate.name.to_string()).fg(Color::Blue),
+                                    Cell::new(adjusted_size.to_string()).fg(Color::Cyan),
+                                ]);
+                            }
+
+                            crates_table
+                        }
+                    }));
+
+                    handles.push(std::thread::spawn(move || -> comfy_table::Table {
+                        let output = std::process::Command::new("cargo")
+                            .arg("bloat")
+                            .arg("--message-format=json")
+                            .output()
                             .unwrap();
 
-                        std::process::exit(1);
+                        let stdout = String::from_utf8(output.stdout).unwrap();
+
+                        let data = serde_json::from_str::<BloatFunctionAnalysis>(&stdout).unwrap();
+
+                        let mut function_table = comfy_table::Table::new();
+
+                        function_table
+                            .set_content_arrangement(ContentArrangement::Dynamic)
+                            .load_preset(UTF8_FULL)
+                            .apply_modifier(UTF8_ROUND_CORNERS);
+
+                        function_table.set_header(vec!["Crate", "Function", "Size"]);
+
+                        for function in data.functions.iter() {
+                            let size = byte_unit::Byte::from_bytes(function.size as u128);
+                            let adjusted_size = size.get_appropriate_unit(true);
+
+                            function_table.add_row(vec![
+                                Cell::new(function.crate_field.to_string()).fg(Color::Blue),
+                                Cell::new(function.name.to_string()),
+                                Cell::new(adjusted_size.to_string()).fg(Color::Cyan),
+                            ]);
+                        }
+
+                        function_table
+                    }));
+
+                    let mut tables: Vec<comfy_table::Table> = vec![];
+
+                    for handle in handles {
+                        tables.push(handle.join().unwrap());
                     }
 
-                    spinner.set_message("Analysing".bright_cyan().to_string());
-
-                    let stdout = String::from_utf8(output.unwrap().stdout).unwrap();
-
-                    let data = serde_json::from_str::<BloatCrateAnalysis>(&stdout).unwrap();
-
-                    let total_size = byte_unit::Byte::from_bytes(data.file_size as u128);
-                    let adjusted_size = total_size.get_appropriate_unit(true);
-
-                    println!("Total Size: {}", adjusted_size.to_string().bright_yellow());
-
-                    let mut crates_table = comfy_table::Table::new();
-
-                    crates_table
-                        .load_preset(UTF8_FULL)
-                        .apply_modifier(UTF8_ROUND_CORNERS)
-                        .set_content_arrangement(ContentArrangement::DynamicFullWidth);
-
-                    crates_table.set_header(vec!["Name", "Size"]);
-
-                    for _crate in data.crates.iter() {
-                        let size = byte_unit::Byte::from_bytes(_crate.size as u128);
-                        let adjusted_size = size.get_appropriate_unit(true);
-
-                        crates_table.add_row(vec![
-                            Cell::new(_crate.name.to_string()).fg(Color::Blue),
-                            Cell::new(adjusted_size.to_string()).fg(Color::Cyan),
-                        ]);
+                    for table in tables {
+                        println!("{table}");
                     }
-
-                    let output = std::process::Command::new("cargo")
-                        .arg("bloat")
-                        .arg("--message-format=json")
-                        .output()
-                        .unwrap();
-
-                    let stdout = String::from_utf8(output.stdout).unwrap();
-
-                    let data = serde_json::from_str::<BloatFunctionAnalysis>(&stdout).unwrap();
-
-                    let mut function_table = comfy_table::Table::new();
-
-                    function_table
-                        .set_content_arrangement(ContentArrangement::Dynamic)
-                        .load_preset(UTF8_FULL)
-                        .apply_modifier(UTF8_ROUND_CORNERS);
-
-                    function_table.set_header(vec!["Crate", "Function", "Size"]);
-
-                    for function in data.functions.iter() {
-                        let size = byte_unit::Byte::from_bytes(function.size as u128);
-                        let adjusted_size = size.get_appropriate_unit(true);
-
-                        function_table.add_row(vec![
-                            Cell::new(function.crate_field.to_string()).fg(Color::Blue),
-                            Cell::new(function.name.to_string()),
-                            Cell::new(adjusted_size.to_string()).fg(Color::Cyan),
-                        ]);
-                    }
-
-                    spinner.finish_and_clear();
-
-                    println!("{crates_table}");
-                    println!("{function_table}");
 
                     println!(
-                        "{}: All sizes shown are estimates and will not be 100% accurate.",
+                        "\n{}: All sizes shown are estimates and will not be 100% accurate.",
                         "Note".bright_yellow()
                     );
                 }
